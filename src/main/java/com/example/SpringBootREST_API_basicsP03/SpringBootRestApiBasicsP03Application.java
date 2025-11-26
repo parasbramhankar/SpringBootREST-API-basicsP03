cd5a9be14ef66d6df17409b7d9f997eceee3d99f
parasbramhankar <parasbramhankar.in@gmail.com>
2025-11-26 11:38:18 +0530

    Initial commit

---
+++ src/main/java/com/example/SpringBootREST_API_basicsP03/SpringBootRestApiBasicsP03Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootREST_API_basicsP03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiBasicsP03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiBasicsP03Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootREST_API_basicsP03/SpringBootRestApiBasicsP03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootREST_API_basicsP03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiBasicsP03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
