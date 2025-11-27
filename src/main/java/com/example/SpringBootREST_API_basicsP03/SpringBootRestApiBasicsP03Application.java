4a2c08a5fdba6bf1675e16fd4402deaa3f04562a
parasbramhankar <parasbramhankar.in@gmail.com>
2025-11-27 21:00:03 +0530

    Commit

--- src/main/java/com/example/SpringBootREST_API_basicsP03/SpringBootRestApiBasicsP03Application.java
+++ src/main/java/com/example/SpringBootREST_API_basicsP03/SpringBootRestApiBasicsP03Application.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootRestApiBasicsP03Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootRestApiBasicsP03Application.class, args);
-	}
+
+        SpringApplication.run(SpringBootRestApiBasicsP03Application.class, args);
+
+    }
 
 }
